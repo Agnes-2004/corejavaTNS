4d071523fb49a8c815e6bfd55290fe06b3d0935b
Home <Home@DESKTOP-K7U3OOK>
2024-03-05 02:15:19 -0800

    This is an java core basic program practice

---
+++ corejavaTNS/src/day_1/Addj.java
@@ -0,0 +1,20 @@
+package day_1;
+
+import java.util.Scanner;
+
+public class Addj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s= new Scanner(System.in);
+
+System.out.println("Enter the value of A:");
+ int a =s.nextInt();
+System.out.println("Enter the value of B:");
+int b=s.nextInt();
+int c=a+b;
+System.out.println("The result="+c);
+ 
+	}
+
+}
---
+++ corejavaTNS/src/day_1/Samplejava.java
@@ -0,0 +1,10 @@
+package day_1;
+
+public class Samplejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HII GUYS.....");
+	}
+
+}
