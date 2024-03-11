fda2af5b26c5ec20cf30981dd60514fa9cec2554
Home <Home@DESKTOP-K7U3OOK>
2024-03-11 03:20:27 -0700

    hello pro

---
+++ corejavaTNS/src/day_4/user1.java
@@ -0,0 +1,17 @@
+package day_4;
+
+import java.util.Scanner;
+
+public class user1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			Scanner input=new Scanner(System.in);
+			System.out.println("Enter your name:");
+			String n=input.nextLine();
+			System.out.println("Hello"+n);
+			}
+	}
+
+}
