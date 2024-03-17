234d03013371d5319a923f805c9f2d1eabe06661
Home <Home@DESKTOP-K7U3OOK>
2024-03-16 23:28:51 -0700

    The single inheritance for both citizen and student class

---
+++ corejavaTNS/src/day_5/Single_Inheritance.java
@@ -0,0 +1,11 @@
+package day_5;
+
+public class Single_Inheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		student Student = new student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(Student);
+	}
+
+}
